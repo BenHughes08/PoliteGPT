b829fbe54cb4e8646aabbcf3b6f39817fbe3a55e
benwhoisjammin <ringesmc@gmail.com>
2024-06-24 22:02:41 +0100

    updated popup

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var button = document.getElementById('myButton');
-  button.addEventListener('click', function () {
-    alert('Button clicked!');
+document.getElementById('changeColor').addEventListener('click', () => {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    browser.tabs.executeScript(tabs[0].id, {
+      code: 'document.body.style.backgroundColor = "lightblue";'
+    });
   });
 });
