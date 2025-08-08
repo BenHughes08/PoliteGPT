ae31de1c9878a9dfb3746ba7992faf1b1d046c60
BenHughes08 <ringesmc@gmail.com>
2025-08-08 13:01:50 +0100

    Delete firefox directory

--- firefox/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('changeColor').addEventListener('click', () => {
-  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-    browser.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "lightblue";'
-    });
-  });
-});
